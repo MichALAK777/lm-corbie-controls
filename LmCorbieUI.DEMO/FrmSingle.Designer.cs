e941edd37d2467c9d3c2a5f843a1fc379df421a3
michalakleo <michalakleo@gmail.com>
2023-02-06 12:29:41 -0300

    first commit

---
+++ LmCorbieUI.DEMO/FrmSingle.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace LmCorbieUI.DEMO
+{
+    partial class FrmSingle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmSingle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 328);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "FrmSingle";
+            this.Padding = new System.Windows.Forms.Padding(2, 30, 2, 2);
+            this.Text = "Formulário Simples";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ LmCorbieUI/05_LmDesign/LmDefEnums.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LmCorbieUI.Design
+{
+    public enum LmValueType
+    {
+        Alfanumerico = 0,
+        Num_Inteiro = 1,
+        Num_Real = 2,
+        Data = 3,
+        Fone = 4,
+        Monetario = 5,
+        Email = 6,
+        Cpf_Cnpj = 7,
+        Hora = 8,
+        ComboBox = 9,
+        ComboBox_Enum = 10,
+        Senha = 11,
+    }
+
+    public enum LmTransactionType 
+    {
+        Novo = 0,
+        Alterar = 1,
+        Excluir = 2,
+        Login = 3,
+        Diversos = 99,
+    }
+
+    public enum LmControlStatus 
+    { 
+        Normal = 0,
+        Selected = 1,
+        Disabled = 2,
+    }
+
+    public enum ButtonStyle
+    {
+        Default = 0,
+        Information = 1,
+        Question = 2,
+        Warning = 3,
+        Error = 4,
+        Gray = 5,
+    }
+
+}
